026a12a92de84c506553f81ac8bcd36e1d73cae7
sh <shchoi2@netpas.net>
2021-02-04 11:36:04 +0900

    memory card example

---
+++ public/vueComponentExample.js
@@ -0,0 +1,13 @@
+Vue.component('click-counter',
+    {
+        template: '#click-counter-template',
+        data(){
+            return{
+                count : 0
+            }
+        }
+    })
+
+new Vue({
+    el: '#app2'
+})
